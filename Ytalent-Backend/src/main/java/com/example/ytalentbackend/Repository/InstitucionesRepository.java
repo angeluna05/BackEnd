53b833d94a9ff09ed937b86288358d9219841ac9
angeluna05 <109944564+Gaviria15@users.noreply.github.com>
2024-12-11 14:40:46 -0500

    Revert "Normalize line endings"
    This reverts commit c50037a9c5f12f3d39817b5009c3d65ea02947e5.

--- Ytalent-Backend/src/main/java/com/example/ytalentbackend/Repository/InstitucionesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.ytalentbackend.Repository;
-
-import com.example.ytalentbackend.Models.Instituciones;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InstitucionesRepository extends JpaRepository<Instituciones, Integer> {
-}
--- Ytalent-Backend/src/main/java/com/example/ytalentbackend/Repository/RetosRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.ytalentbackend.Repository;
-
-import com.example.ytalentbackend.Models.Retos;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RetosRepository extends JpaRepository<Retos, Integer> {
-}
